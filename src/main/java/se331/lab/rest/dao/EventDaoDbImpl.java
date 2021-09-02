7199991125e40bbbdbd464115f11da1af9518e09
Kitsada <kitsada_t@cmu.ac.th>
2021-09-02 22:43:17 +0700

    3.7

--- src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
+++ src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
@@ -1,11 +1,14 @@
 package se331.lab.rest.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
 import se331.lab.rest.entity.Event;
 import se331.lab.rest.repository.EventRepository;
 
 import java.util.List;
-
+@Repository
+@Profile("db")
 public class EventDaoDbImpl implements EventDao {
     @Autowired
     EventRepository eventRepository;
